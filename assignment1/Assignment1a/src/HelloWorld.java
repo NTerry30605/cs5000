80ec63bfe70be05f0e07d6e8b34a74a6fb048a3e
nikko <nterry9@students.kennesaw.edu>
2018-01-29 21:47:45 -0500

    assignment 1a/program 1

--- assignment1/Assignment1a/src/HelloWorld.java
+++ assignment1/Assignment1a/src/HelloWorld.java
@@ -1,4 +1,10 @@
-/**
+import java.util.Scanner;
+
+/**CS 5000/W01
+ * Spring 2018
+ * Nikko Terry
+ * Dr. Haddad
+ * Assignment 1
  * 
  */
 
@@ -8,12 +14,15 @@
  */
 public class HelloWorld {
 
-	/**
-	 * @param args
+	/**Problem #1
+	 * 
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner s = new Scanner(System.in);
+		 System.out.print("Please enter your name: ");
+		 String name = s.nextLine();
+		 System.out.println("Hello " + name + "!");
 	}
 
 }
